a1ac027be1d77d9c930e9cc23421152585be89db
Sameeksha-p <141543290+Sameeksha-p@users.noreply.github.com>
2024-09-12 17:03:57 +0530

    Time: 3 ms (46.62%), Space: 8.6 MB (13.23%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.c
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -1,14 +1,23 @@
 bool isValid(char *s) {
-
-    char *q=s;
-    
-    for (char *p=s; *p; p++) 
-        switch(*p) {
-            case '(': *q++ = ')'; continue;
-            case '{': *q++ = '}'; continue;
-            case '[': *q++ = ']'; continue;
-            default: if (q==s || *p != *--q) return false;
+   char stack[10000];
+    int top=-1;
+    for (int i=0;s[i]!='\0';i++){
+        char c=s[i];
+        if(c=='('||c=='{'||c=='['){
+            stack[++top]=c;
         }
-    
-    return q==s;
+        else{
+            if (top==-1){
+                return false;
+            }
+            switch(c){
+                case ')': if (stack[top]!='(')return false;break;
+                 case '}': if (stack[top]!='{')return false;break;
+                     case ']': if (stack[top]!='[')return false;break;
+            }
+            top--;
+            
+}
+}
+    return top ==-1;
 }
