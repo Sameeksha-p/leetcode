b14e2c6fadaa410e8f4f27cb83fed183b9a07a2a
Sameeksha-p <141543290+Sameeksha-p@users.noreply.github.com>
2024-09-17 11:53:01 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (42.45%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.c
@@ -0,0 +1,54 @@
+
+
+
+typedef struct {
+   int *arr;
+    int front;
+    int rear;
+    int capacity
+} MyQueue;
+
+
+MyQueue* myQueueCreate() {
+    MyQueue* obj=(MyQueue*)malloc(sizeof(MyQueue));
+    obj->arr=(int*)malloc(sizeof(int)*100);
+    obj->front=0;
+    obj->rear=-1;
+    return obj;
+    
+}
+
+void myQueuePush(MyQueue* obj, int x) {
+    obj->arr[++obj->rear]=x;
+}
+
+int myQueuePop(MyQueue* obj) {
+    return obj->arr[obj->front++];
+}
+
+int myQueuePeek(MyQueue* obj) {
+    return obj->arr[obj->front];
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+    return obj->front> obj->rear;
+}
+
+void myQueueFree(MyQueue* obj) {
+    free(obj->arr);
+    free(obj);
+}
+
+/**
+ * Your MyQueue struct will be instantiated and called as such:
+ * MyQueue* obj = myQueueCreate();
+ * myQueuePush(obj, x);
+ 
+ * int param_2 = myQueuePop(obj);
+ 
+ * int param_3 = myQueuePeek(obj);
+ 
+ * bool param_4 = myQueueEmpty(obj);
+ 
+ * myQueueFree(obj);
+*/
